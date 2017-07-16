5a22b0d2cfabb2a9c9f039ab1ca7e558ed654834
Dana Johnson <dnjohnson84@gmail.com>
2017-07-16 11:38:12 -0700

    Add support for collections

--- lib/jekyll-pandoc-multiple-formats/generator.rb
+++ lib/jekyll-pandoc-multiple-formats/generator.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2012-2015 Nicolás Reynolds <fauno@endefensadelsl.org>
 #               2012-2013 Mauricio Pasquier Juan <mpj@endefensadelsl.org>
 #               2013      Brian Candler <b.candler@pobox.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -27,6 +27,19 @@ class PandocGenerator < Generator
   safe true
 
   attr_accessor :site, :config
+
+  def doc_categories_hash()
+    hash = Hash.new { |h, key| h[key] = [] }
+
+    @site.collections.each do |name, collection|
+      collection.docs.each do |doc|
+        doc.data['categories'].each { |t| hash[t] << doc } if doc.data['categories']
+      end
+    end
+
+    hash.values.each { |posts| posts.sort!.reverse! }
+    hash
+  end
 
   def generate(site)
     @site     ||= site
@@ -47,9 +60,31 @@ class PandocGenerator < Generator
         @pandoc_files << pandoc_file
       end
 
-      @site.post_attr_hash('categories').each_pair do |title, posts|
-        posts.sort!
-        pandoc_file = PandocFile.new(@site, output, posts, title)
+      @site.collections.each do |name, collection|
+        collection.docs.each do |doc|
+          pandoc_file = PandocFile.new(@site, output, doc)
+          next unless pandoc_file.write
+
+          @site.keep_files << pandoc_file.relative_path
+          @pandoc_files << pandoc_file
+        end
+      end
+
+      def categories
+        if Jekyll::VERSION >= '3.0.0'
+          doc_categories_hash()
+        else
+          @site.post_attr_hash('categories')
+        end
+      end
+
+      categories.each_pair do |title, docs|
+
+        sorted_docs = docs.sort_by { | doc |
+          doc.data["order"] || 10000
+        }
+
+        pandoc_file = PandocFile.new(@site, output, sorted_docs, title)
 
         if @site.keep_files.include? pandoc_file.relative_path
           puts "#{pandoc_file.relative_path} is a category file AND a post file"
